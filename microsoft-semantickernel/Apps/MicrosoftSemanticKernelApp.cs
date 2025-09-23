dc551bea5a6ff3c8fe1bfc3cbb41a5c3926ddc90
Pierre Michel <pmiche04@gmail.com>
2025-09-23 19:09:18 +0200

    Using UseEffect for the task extraction in order to not block the ui. Removing the record since ToForm() is not needed. Using an array instead of html

--- microsoft-semantickernel/Apps/MicrosoftSemanticKernelApp.cs
+++ microsoft-semantickernel/Apps/MicrosoftSemanticKernelApp.cs
@@ -1,5 +1,7 @@
 
 
+using Ivy;
+using Ivy.Core.Hooks;
 using Microsoft.SemanticKernel;
 using System.Net;
 
@@ -8,37 +10,61 @@ namespace MicrosoftSemanticKernelDemo.Apps;
 [App(icon: Icons.PartyPopper, title: "MicrosoftSemanticKernel Demo")]
 public class MicrosoftSemanticKernelApp : ViewBase
 {
-    public record NotesModel(
-        string NotesFromMeeting
-    );
-
+    private Kernel kernel;
+    private KernelFunction extractTasks;
+    private IState<string> notesFromMeeting;
+    private IState<string[]> tasks;
+    private IState<int> triggerRefresh;
     public override object? Build()
     {
         //initialize Open Ai
         //***  REPLACE API_KEY WITH YOUR OPENAI APIKEY  ***
-        var kernel = Kernel.CreateBuilder()
+        kernel = Kernel.CreateBuilder()
             .AddOpenAIChatCompletion("gpt-4o-mini", "API_KEY")
             .Build();
 
         // Create the function for extracting an action list from meeting notes
-        var extractTasks = kernel.CreateFunctionFromPrompt("Extract action items as a list without title \n{{$input}}");
+        extractTasks = kernel.CreateFunctionFromPrompt("Extract action items as a list without title \n{{$input}}");
 
-        // Use a model for the meetings notes to be able to use .ToForm() method
-        var notesModel = UseState(() => new NotesModel("Today we discussed project deadlines. John will send the report tomorrow. Maria will set up the next meeting."));
-
-        // Execute the extraction function with the notes that come from the text area.
-        var tasksString = kernel.InvokeAsync(extractTasks, new() { ["input"] = notesModel.Value.NotesFromMeeting }).Result.GetValue<string>().Replace("\n", "<br/>");
+        notesFromMeeting = UseState(() => "Today we discussed project deadlines. John will send the report tomorrow. Maria will set up the next meeting.");
         
+        triggerRefresh = UseState(0);
+        tasks = UseState<string[]>([]);
+        
+        // Execute the extraction function asynchronously
+        UseEffect(async () =>
+        {
+            var result = await ExtractTasksFromNoteMeeting();
+            tasks.Set(result);
+
+        }, triggerRefresh,EffectTrigger.AfterInit());
+        
+
+
         return Layout.Horizontal()
             | new Card(
                 Layout.Vertical()
-                    | notesModel.ToForm()
-                        .Builder(m => m.NotesFromMeeting, s => s.ToTextAreaInput())  
+                    | notesFromMeeting.ToTextAreaInput()
+                    | new Button("Update tasks").HandleClick(_ => UpdateMeetingTasks())
             ).Title("Input").Width(1 / 2f)
             | new Card(
                 Layout.Vertical()
                     | Text.H3("Tasks")
-                    | new Html (tasksString)
+                    | tasks.Value
             ).Title("Result").Width(1 / 2f);
+    }
+
+    private void UpdateMeetingTasks()
+    {
+        triggerRefresh.Set(triggerRefresh.Value + 1);
+    }
+
+    private Task<string[]> ExtractTasksFromNoteMeeting()
+    {
+        return Task.Run(async () =>
+        {
+            var extractedTasks = await kernel.InvokeAsync(extractTasks, new() { ["input"] = notesFromMeeting });
+            return extractedTasks.GetValue<string>().Split('\n');
+        });
     }
 }
