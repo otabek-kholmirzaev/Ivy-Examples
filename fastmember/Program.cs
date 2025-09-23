a5e19f1147ed2edb9390a344c4f10fd6e2d19a1e
greatPurpose <senior.fullstack.engineer@gmail.com>
2025-09-23 12:21:12 -0700

    feat(issue-24): make the app as default app

--- fastmember/Program.cs
+++ fastmember/Program.cs
@@ -1,3 +1,4 @@
+using FastMemberDemo.Apps;
 
 CultureInfo.DefaultThreadCurrentCulture = CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("en-US");
 var server = new Server();
@@ -6,6 +7,6 @@ server.UseHotReload();
 #endif
 server.AddAppsFromAssembly();
 server.AddConnectionsFromAssembly();
-var chromeSettings = new ChromeSettings().UseTabs(preventDuplicates: true);
+var chromeSettings = new ChromeSettings().DefaultApp<FastMemberDemoApp>().UseTabs(preventDuplicates: true);
 server.UseChrome(chromeSettings);
 await server.RunAsync();
