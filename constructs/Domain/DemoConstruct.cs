1723ce3fab95f205894331ae4fcd159051768984
Vadim Belov <bvdcode@users.noreply.github.com>
2025-09-24 18:31:39 -0700

    Refactor DemoConstruct with new properties and methods
    Updated namespace and inheritance for DemoConstruct.
    Added properties for child constructs and modified the constructor to initialize them.
    Introduced a static method to build a root construct.

--- constructs/Domain/DemoConstruct.cs
+++ constructs/Domain/DemoConstruct.cs
@@ -1,8 +1,29 @@
-using Constructs;
+﻿using Constructs;
 
-namespace Ivy.Examples.Constructs;
+namespace Ivy.Examples.Constructs.Domain;
 
-public class DemoConstruct
+public class DemoConstruct : Construct
 {
+    public Construct ChildA { get; }
+    public Construct ChildB { get; }
+    public Construct Nested { get; }
+    public Construct Leaf1 { get; }
+    public Construct Leaf2 { get; }
 
+    public DemoConstruct(Construct scope, string id) : base(scope, id)
+    {
+        ChildA = new Construct(this, "ChildA");
+        ChildB = new Construct(this, "ChildB");
+
+        Nested = new Construct(this, "Nested");
+        Leaf1 = new Construct(Nested, "Leaf1");
+        Leaf2 = new Construct(Nested, "Leaf2");
+    }
+
+    public static RootConstruct BuildRoot()
+    {
+        var root = new RootConstruct("Root");
+        _ = new DemoConstruct(root, "Demo");
+        return root;
+    }
 }
